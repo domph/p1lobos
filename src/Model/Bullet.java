db38aa24dd63fdb9c9074a731e4a18c0d9628980
banditcat12 <banditcat12@gmail.com>
2021-01-21 08:21:08 -0800

    test

--- src/Model/Bullet.java
+++ src/Model/Bullet.java
@@ -5,8 +5,8 @@ import java.awt.*;
 public class Bullet extends GameObject {
     public static final double SIZE_X = 10;
     public static final double SIZE_Y = 10;
-    public static final double BULLET_SPEED = 500;
-    private static final Color DEFAULT_COLOR = new Color(100, 0, 0);
+    public static final double BULLET_SPEED = 750;
+    private static final Color DEFAULT_COLOR = new Color(100, 60, 0);
 
     public Bullet(double PosX, double PosY) {
         super(PosX, PosY, SIZE_X, SIZE_Y, DEFAULT_COLOR);
